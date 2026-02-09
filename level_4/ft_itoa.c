9c4cc498fd26821b8ced3b9de1f5a0b34414c64c
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-09 17:07:38 +0100

    mostly do it

--- level_4/ft_itoa.c
+++ level_4/ft_itoa.c
@@ -1,22 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1,&c,1);
-}
-
-void	ft_putnbr(int nb)
-{
-	if (nb < 0)
-	{
-		write(1,"-",1);
-		nb = nb * -1;
-	}
-	if (nb > 10)
-		ft_putnbr(nb / 10);
-	ft_putchar((nb % 10) + '0');
-}
+#include <stdio.h>
+#include <string.h>
 
 int	count_nbr(int	nbr)
 {
@@ -43,9 +28,18 @@ char	*ft_itoa(int nbr)
 	int total_nbr ;
 	char *str ;
 	total_nbr = count_nbr(nbr);
-	str = malloc(total_nbr + 1);
 	str[total_nbr] = '\0';
-	i = total_nbr - 1;
+	i = total_nbr - 1;	
+	str = malloc(total_nbr + 1);
+	if (str == NULL)
+		return NULL;
+	if (nbr == 0)
+		str[0] = '0';
+	else if (nbr < 0)
+	{
+		str[0] = '-';
+		nbr = nbr * -1;
+	}
 	while (nbr != 0)
 	{
 		str[i] = (nbr % 10) + '0';
@@ -57,6 +51,8 @@ char	*ft_itoa(int nbr)
 
 int	main()
 {
-	char *	str = ft_itoa(1234);
-	write(1,str,4);
+	int nb;
+	scanf("%d",&nb);
+	char *	str = ft_itoa(nb);
+	write(1,str,strlen(str));
 }
